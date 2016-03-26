20d0bd260be869a97da5de6bc8f9240e55b7b72a
philo-grammer <great1106@gmail.com>
2016-03-27 00:08:28 +0900

    [jpa] chap11. 상품기능
    - repository
    - service

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,7 +1,32 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 /**
  * Created by sykim on 2016. 3. 26..
  */
+@Service
+@Transactional
 public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
 }
