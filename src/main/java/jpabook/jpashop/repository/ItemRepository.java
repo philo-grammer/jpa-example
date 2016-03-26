a99b746acbe35dda51ff1cad39155dbb8200bb17
philo-grammer <great1106@gmail.com>
2016-03-26 23:03:59 +0900

    [jpa] chap11. 회원기능
    * repository
    * service
    * test

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop.repository;
+
+/**
+ * Created by sykim on 2016. 3. 26..
+ */
+public interface ItemRepository {
+}
---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,7 @@
+package jpabook.jpashop.service;
+
+/**
+ * Created by sykim on 2016. 3. 26..
+ */
+public class ItemService {
+}
